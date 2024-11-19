ec2dbd957e4c7ba42066d9e238548df70422d9a7
Hermsies <Gijshermsen19@gmail.com>
2024-11-19 11:20:44 +0100

    database aangemaakt

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -11,11 +11,18 @@
 
 import sqlite3
 with sqlite3.connect("MCPizzeria.db") as db:
-    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
+    cursor = db.cursor()
+    #cursor is object waarmee je data uit de database kan halen
 
 
 ### ---------  Functie definities  -----------------
-
+def maakTabellenAan () :
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbi_pizzas(
+        gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+        gerechtNaam TEXT NOT NULL,
+        gerechtPrijs REAL NOT NULL);""")
+    print("Tabel 'tbi_pizzas' aangemaakt")
 
 ### --------- Hoofdprogramma  ---------------
-
+maakTabellenAan()
