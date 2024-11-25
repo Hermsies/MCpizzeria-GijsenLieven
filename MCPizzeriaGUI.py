01a7a4440e3aa2e30fc9120dc5b5ce9eaf276033
Hermsies <Gijshermsen19@gmail.com>
2024-11-25 12:55:38 +0100

    WELKOM TEKST

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -22,8 +22,9 @@ venster = Tk()
 venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
 venster.wm_title("MC Pizzeria")
 knopSluit = Button(venster, text="Venster sluiten", width=50, command=venster.destroy)
-knopSluit.grid(row=0, column=0)
-
+knopSluit.grid(row=17, column=4)
+labelIntro = Label (venster, text="Welkom!")
+labelIntro.grid(row=0, column=0, sticky='W')
 
 
 
